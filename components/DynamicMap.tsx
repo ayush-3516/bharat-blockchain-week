00422ed86ced33a750494f20eabfcc23dc790c8e
Rishi <riship4611@gmail.com>
2023-12-02 15:29:38 +0530

    improved map

--- components/DynamicMap.tsx
+++ components/DynamicMap.tsx
@@ -1,7 +1,10 @@
-import { FC, useEffect } from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { FC, useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { MdDateRange } from "react-icons/md";
+import { CgNametag } from "react-icons/cg";
+
 
 interface Center {
     lat: number;
@@ -9,44 +12,142 @@ interface Center {
 }
 
 export interface EventType {
-    geolocation: { lat: number, lng: number },
-    organizerContact: string,
-    followed: boolean,
-    _id: string,
-    Date: 4,
-    date: string,
-    startTime: string,
-    endTime: string,
-    title: string,
-    category: string,
-    locationUrl: string,
-    entry: string,
-    registration: string,
-    organizer: string
+    geolocation: { lat: number; lng: number };
+    organizerContact: string;
+    followed: boolean;
+    _id: string;
+    Date: 4;
+    date: string;
+    startTime: string;
+    endTime: string;
+    title: string;
+    category: string;
+    locationUrl: string;
+    entry: string;
+    registration: string;
+    organizer: string;
 }
 
 interface MapProps {
     locations: EventType[];
 }
 
-const DynamicMap: FC<MapProps> = ({ locations }) => {
-    const handleMarkerClick = (location: any) => {
-        const { lat, lng } = location.geolocation;
-        const googleMapsUrl = `https://www.google.com/maps?q=${lat},${lng}`;
-        window.open(googleMapsUrl, '_blank');
-    };
+interface MarkerProps {
+    location: EventType;
+    handleMarkerClick: (location: EventType) => void;
+    handleMarkerHover: (location: EventType) => void;
+    handleMarkerLeave: () => void;
+    isHovered: boolean;
+}
 
+function formatDate(inputDate: string) {
+    const date = new Date(inputDate);
+
+    const months = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ];
+
+    const day = date.getUTCDate();
+    const month = months[date.getUTCMonth()];
+    const year = date.getUTCFullYear();
+
+    const formattedDate = `${day} ${month}. ${year}`;
+
+    return formattedDate;
+}
+
+const toolTipStyle = {
+    fontFamily: "Kanit, sans-serif"
+}
+
+const titleFontStyle = {
+    fontSize: "large",
+    display: "flex",
+    alignItems: "center",
+    gap: "10px"
+}
+
+const dateFontStyle = {
+    fontSize: "small",
+    display: "flex",
+    alignItems: "center",
+    gap: "10px"
+}
+
+const CustomMarker: FC<MarkerProps> = ({
+    location,
+    handleMarkerClick,
+    handleMarkerHover,
+    handleMarkerLeave,
+    isHovered,
+}) => {
     const customIcon = new L.Icon({
-        iconUrl: "https://i.imgur.com/sjgnRay.png",
+        iconUrl:"https://i.imgur.com/htyZSPO.png",
         iconSize: [40, 40],
         iconAnchor: [16, 32],
         popupAnchor: [0, -32],
     });
 
+    return (
+        <Marker
+            position={[location?.geolocation.lat, location?.geolocation.lng]}
+            icon={customIcon}
+            eventHandlers={{
+                click: () => handleMarkerClick(location),
+                mouseover: () => handleMarkerHover(location),
+                mouseout: () => handleMarkerLeave(),
+            }}
+
+        >
+            {isHovered && <div className="marker_popup" style={{
+                position: 'absolute',
+                zIndex: 999,
+                padding: '5px',
+                background: 'white',
+                borderRadius: '5px',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+                bottom: 0
+            }}>
+                {location?.title}
+            </div>}
+            <Tooltip >
+                <div style={toolTipStyle}>
+                    <p style={titleFontStyle}>
+                        <CgNametag style={{ fontSize: "large" }} />{location?.title}
+                    </p>
+                    <p style={dateFontStyle}>
+                        <MdDateRange style={{ fontSize: "large" }} /> {formatDate(location.date)}
+                    </p>
+                </div>
+            </Tooltip>
+        </Marker>
+    );
+};
+
+const DynamicMap: FC<MapProps> = ({ locations }) => {
+    const [hoveredLocation, setHoveredLocation] = useState<EventType | null>(
+        null
+    );
+
+    const handleMarkerClick = (location: EventType) => {
+        const { lat, lng } = location.geolocation;
+        const googleMapsUrl = `https://www.google.com/maps?q=${lat},${lng}`;
+        window.open(googleMapsUrl, "_blank");
+    };
+
+    const handleMarkerHover = (location: EventType) => {
+        setHoveredLocation(location);
+    };
+
+    const handleMarkerLeave = () => {
+        setHoveredLocation(null);
+    };
+
     const mapContainerStyle = {
         width: "100%",
         height: "720px",
-        borderRadius: "11px"
+        borderRadius: "11px",
     };
 
     const center: Center = {
@@ -70,15 +171,14 @@ const DynamicMap: FC<MapProps> = ({ locations }) => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 {locations?.map((location: EventType, index: number) => (
-                    <Marker
+                    <CustomMarker
                         key={index}
-                        position={[location?.geolocation.lat, location?.geolocation.lng]}
-                        icon={customIcon}
-                        eventHandlers={{
-                            click: () => handleMarkerClick(location),
-                        }}
-                    >
-                    </Marker>
+                        location={location}
+                        handleMarkerClick={handleMarkerClick}
+                        handleMarkerHover={handleMarkerHover}
+                        handleMarkerLeave={handleMarkerLeave}
+                        isHovered={hoveredLocation === location}
+                    />
                 ))}
             </MapContainer>
         </div>
